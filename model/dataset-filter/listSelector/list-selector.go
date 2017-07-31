f38735c943bab24a37664fccd78b8ad9f07fcba6
boxadesign <info@boxadesign.co.uk>
2017-07-31 15:57:19 +0100

    Updated model for age range list to be generic for any range list

--- model/dataset-filter/listSelector/list-selector.go
+++ model/dataset-filter/listSelector/list-selector.go
@@ -1,23 +1,24 @@
-package ageSelectorList
+package listSelector
 
 import "github.com/ONSdigital/dp-frontend-models/model"
 
 // Page ...
 type Page struct {
 	model.Page
-	Data  AgeSelectorList `json:"data"`
-	JobID string          `json:"job_id"`
+	Data     ListSelector `json:"data"`
+	FilterID string       `json:"job_id"`
 }
 
-// AgeSelectorList ...
-type AgeSelectorList struct {
+// ListSelector ...
+type ListSelector struct {
 	AddFromRange  Link     `json:"add_from_range"`
 	SaveAndReturn Link     `json:"save_and_return"`
 	Cancel        Link     `json:"cancel"`
 	FiltersAmount int      `json:"filters_amount"`
 	FiltersAdded  []Filter `json:"filters_added"`
+	AddAllInRange Link     `json:"add_all"`
 	RemoveAll     Link     `json:"remove_all"`
-	AgeRange      Range    `json:"age_range"`
+	RangeData     Range    `json:"range_values"`
 }
 
 // Link ...
@@ -34,6 +35,9 @@ type Filter struct {
 
 // Range ...
 type Range struct {
-	StartNum int `json:"start_num"`
-	EndNum   int `json:"end_num"`
+	StartNum     int    `json:"start_num"`
+	EndNum       int    `json:"end_num"`
+	StartLabel   string `json:"start_label"`
+	EndLabel     string `json:"end_label"`
+	AppendString string `json:"append_string"`
 }
