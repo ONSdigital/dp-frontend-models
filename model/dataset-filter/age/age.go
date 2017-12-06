f4ce5f4c4fcc1caeb9715eba2a0cdcd75c07531d
mattrout92 <matt_rout92@live.co.uk>
2017-12-06 13:58:11 +0000

    Add age model

---
+++ model/dataset-filter/age/age.go
@@ -0,0 +1,32 @@
+package age
+
+import "github.com/ONSdigital/dp-frontend-models/model"
+
+// Page represents an age selection page
+type Page struct {
+	model.Page
+	Data     Data   `json:"data"`
+	FilterID string `json:"filter_id"`
+}
+
+// Data represents the data for the age page
+type Data struct {
+	Youngest     string  `json:"youngest"`
+	Oldest       string  `json:"oldest"`
+	Ages         []Value `json:"ages"`
+	CheckedRadio string  `json:"checked_radio"`
+	FormAction   Link    `json:"form_action"`
+}
+
+// Value represents a single age value
+type Value struct {
+	Label      string
+	Option     string
+	IsSelected bool
+}
+
+// Link represents a link
+type Link struct {
+	Label string `json:"label"`
+	URL   string `json:"url"`
+}
