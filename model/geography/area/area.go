482ce1d1b207b0630afb551efcbcbbb671b70cf5
Jon Dewi Jones <jondewijones@gmail.com>
2018-09-19 13:03:55 +0100

    Add models for geography area page

--- model/geography/area/area.go
+++ model/geography/area/area.go
@@ -10,4 +10,20 @@ type Page struct {
 
 // GeographyAreaPage represents the data specific to an area page
 type GeographyAreaPage struct {
+	Datasets   []Dataset `json:"items"`
+	Attributes Attribute `json:"attributes"`
+}
+
+// Dataset represents each dataset related to specific code
+type Dataset struct {
+	Label       string `json:"label"`
+	Description string `json:"description"`
+	ID          string `json:"id"`
+	URI         string `json:"uri"`
+}
+
+// Attribute represents each attribute related to specific code
+type Attribute struct {
+	Code        string `json:"code"`
+	ReleaseDate string `json:"release_date"`
 }
