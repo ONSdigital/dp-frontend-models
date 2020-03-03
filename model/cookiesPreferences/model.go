e83eb22fe44d4bf8c4037b3b2d6b9d074d2ad955
Ravi Pradhan <ravipradhan@protonmail.com>
2020-03-03 08:27:25 +0000

    added  for json key name

--- model/cookiesPreferences/model.go
+++ model/cookiesPreferences/model.go
@@ -5,5 +5,5 @@ import "github.com/ONSdigital/dp-frontend-models/model"
 // Page model data for the cookies preferences form
 type Page struct {
 	model.Page
-	PreferencesUpdated bool `json:preferences_updated`
+	PreferencesUpdated bool `json:"preferences_updated"`
 }
