3102299d1e3c3d8f3411141c43f6c966f0d54e09
Ravi <ravipradhan@protonmail.com>
2021-02-10 09:19:19 +0000

    extract pagination from search package and move to model. Update filter-dataset and search page models

---
+++ model/pagination.go
@@ -0,0 +1,10 @@
+package model
+
+// Pagination represents all information regarding pagination of search results
+type Pagination struct {
+	CurrentPage    int    `json:"page,omitempty"`
+	TotalPages     int    `json:"total_pages,omitempty"`
+	URLWithoutPage string `json:"url_without_page,omitempty"`
+	Limit          int    `json:"limit,omitempty"`
+	LimitOptions   []int  `json:"limit_options,omitempty"`
+}
