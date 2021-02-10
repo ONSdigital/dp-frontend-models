e6ae473d6c88a1475fbcb489a31e620d798b46d1
Ravi <ravipradhan@protonmail.com>
2021-02-10 09:21:34 +0000

    update CurrentPage tag

--- model/pagination.go
+++ model/pagination.go
@@ -2,7 +2,7 @@ package model
 
 // Pagination represents all information regarding pagination of search results
 type Pagination struct {
-	CurrentPage    int    `json:"page,omitempty"`
+	CurrentPage    int    `json:"current_page,omitempty"`
 	TotalPages     int    `json:"total_pages,omitempty"`
 	URLWithoutPage string `json:"url_without_page,omitempty"`
 	Limit          int    `json:"limit,omitempty"`
