b71965887ba3e503a0a06943238ab3d9686930bb
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2021-02-11 12:27:14 +0000

    remove URLWithoutPage as already given in PageToDisplay.URL

--- model/pagination.go
+++ model/pagination.go
@@ -5,7 +5,6 @@ type Pagination struct {
 	CurrentPage    int             `json:"current_page"`
 	PagesToDisplay []PageToDisplay `json:"pages_to_display"`
 	TotalPages     int             `json:"total_pages"`
-	URLWithoutPage string          `json:"url_without_page"`
 	Limit          int             `json:"limit"`
 	LimitOptions   []int           `json:"limit_options,omitempty"`
 }
