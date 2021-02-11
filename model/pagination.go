6f3b71aa545bc2548d7aa75a349ed6d698f2c340
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2021-02-11 10:21:52 +0000

    add pagination pages

--- model/pagination.go
+++ model/pagination.go
@@ -3,6 +3,7 @@ package model
 // Pagination represents all information regarding pagination of search results
 type Pagination struct {
 	CurrentPage    int    `json:"current_page,omitempty"`
+	Pages          []int  `json:"pages,omitempty"`
 	TotalPages     int    `json:"total_pages,omitempty"`
 	URLWithoutPage string `json:"url_without_page,omitempty"`
 	Limit          int    `json:"limit,omitempty"`
