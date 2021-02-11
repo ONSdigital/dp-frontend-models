ada0a5efcd7b130c8bdc02bfac9c26bdc839e972
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2021-02-11 11:23:15 +0000

    add PageToDisplay struct and remove omitempty

--- model/pagination.go
+++ model/pagination.go
@@ -2,10 +2,16 @@ package model
 
 // Pagination represents all information regarding pagination of search results
 type Pagination struct {
-	CurrentPage    int    `json:"current_page,omitempty"`
-	Pages          []int  `json:"pages,omitempty"`
-	TotalPages     int    `json:"total_pages,omitempty"`
-	URLWithoutPage string `json:"url_without_page,omitempty"`
-	Limit          int    `json:"limit,omitempty"`
-	LimitOptions   []int  `json:"limit_options,omitempty"`
+	CurrentPage    int             `json:"current_page"`
+	PagesToDisplay []PageToDisplay `json:"pages_to_display"`
+	TotalPages     int             `json:"total_pages"`
+	URLWithoutPage string          `json:"url_without_page"`
+	Limit          int             `json:"limit"`
+	LimitOptions   []int           `json:"limit_options,omitempty"`
+}
+
+// PageToDisplay represents a page to display in pagination with their corresponding URL
+type PageToDisplay struct {
+	Page int    `json:"page"`
+	URL  string `json:"url"`
 }
